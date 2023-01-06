cc60991dcd10a88c880b6922e349512afbda69e9
Gray <blackcat5523@gmail.com>
2023-01-06 16:34:14 +0800

    安裝套件開始作業

--- src/containers/users/Users.jsx
+++ src/containers/users/Users.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './users.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./users.css";
 
+import { HeatImage } from "./HeatImage";
+import { TitleButtons } from "./TitleButtons";
 const Users = () => (
-  <div id='users'>
-  Users
-  <br />
-    <button className='login-button'><Link to='/users/login' >Login</Link></button>
-  </div>
+    <div id='users'>
+        <HeatImage />
+        <h1>您好 May 你現在是藝拍小夥伴啦！</h1>
+        <TitleButtons
+         icon='?' 
+         word='帳務設定' 
+         />
+
+        {/* <br />
+    <button className='login-button'><Link to='/users/login' >Login</Link></button> */}
+    </div>
 );
 
 export default Users;
