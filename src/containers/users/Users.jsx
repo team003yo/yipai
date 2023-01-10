25f2e0bcdb27d56d43a2210590489bde73e5d7b8
Gray <blackcat5523@gmail.com>
2023-01-10 16:22:28 +0800

    登入頁面架構

--- src/containers/users/Users.jsx
+++ src/containers/users/Users.jsx
@@ -2,19 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./users.css";
 
-import { HeatImage } from "./HeatImage";
-import { TitleButtons } from "./TitleButtons";
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import { HeatImage } from "./HeatImage";
 const Users = () => (
     <div id='users'>
-        <HeatImage />
-        <h1>您好 May 你現在是藝拍小夥伴啦！</h1>
-        <TitleButtons
-         icon='?' 
-         word='帳務設定' 
-         />
-
-        {/* <br />
-    <button className='login-button'><Link to='/users/login' >Login</Link></button> */}
+        {/* <HeatImage /> */}
+        <h2>您好 May 你現在是藝拍小夥伴啦！</h2>
+        <img src={{}} alt="Background" ></img> 
+        
+    <Link to='/users/login' className="btn btn-info" >買家登入</Link>
+    <Link to='/users/login' className="btn btn-primary" >賣家登入</Link>
     </div>
 );
 
