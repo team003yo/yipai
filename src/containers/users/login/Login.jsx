4684f63b9794e52d3d9efb89d4fc18507f0f0135
Gray <blackcat5523@gmail.com>
2023-01-11 16:51:46 +0800

    買家登入畫面好了

--- src/containers/users/login/Login.jsx
+++ src/containers/users/login/Login.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
-import './login.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./login.css";
 
 const Login = () => (
-  <div id='login'>
-    <img src={{}} alt=""></img>
-    
-  </div>
+    <div id='login'>
+        {/* 買家登入 */}
+        <div className='BG'>
+            <div className='flex buyerbox'>
+                <div className='buyerIcon'></div>
+                <form method='POST' action='' className='flex'>
+                    <label className='h3'>買家登入</label>
+                    <input
+                        type='text'
+                        className='inputBox'
+                        name='accountNumber'
+                        placeholder=' 帳號'
+                    />
+                    <input
+                        type='password'
+                        className='inputBox2'
+                        name='password'
+                        placeholder=' 密碼'
+                    />
+                    <button type='submit' className='btn'>
+                        登入
+                    </button>
+                </form>
+                <div style={{ fontColor: "#515151" }}>
+                    沒有會員？請
+                    <span >
+                        <Link to=''>註冊</Link>
+                    </span>
+                </div>
+            </div>
+        </div>
+
+        {/* 賣家登入 */}
+        
+    </div>
 );
 
 export default Login;
