f6e73e691162f03f79b7395abc56dc28b610ab83
Allenkuo <scf0309@yahoo.com.tw>
2023-01-12 12:44:44 +0800

    .

--- src/containers/artist/Artist.jsx
+++ src/containers/artist/Artist.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import  { Container, Dropdown, DropdownButton}  from  'react-bootstrap' ;
+// import Dropdown from 'react-bootstrap/Dropdown';
+// import DropdownButton from 'react-bootstrap/DropdownButton';
 import './artist.css';
 
 const Artist = () => (
-  <div id='artist'>artist</div>
+  <>  
+  <div className="yipai__artist section__padding" id="home">
+    <div className="yipai__artist-image">      
+    </div>    
+  </div>
+  <Container>
+    <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+      <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+      <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+    </DropdownButton>
+  </Container>
+    
+  </>
 );
 
 export default Artist;
