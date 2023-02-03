77955469efcd3b65ad5d1bdfe04254002c384608
y343632 <117623833+y343632@users.noreply.github.com>
2023-02-03 12:36:14 +0800

    Add files via upload

--- src/containers/users/Sell/SellerHome.jsx
+++ src/containers/users/Sell/SellerHome.jsx
@@ -7,19 +7,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import SellerButton from './SellerButton'
 
 // Onclick
-import { SellerFrontPage } from './SellerOnclick'
-import { SellerPage } from './SellerOnclick'
-import { SellerUpload } from './SellerOnclick'
-import { SellerProduct } from './SellerOnclick'
-import { SellerOrder } from './SellerOnclick'
+
+import { SellerFrontPage,SellerPage,SellerUpload,SellerProduct,SellerOrder } from './SellerOnclick'
+
+
 
 // icon
-import buyerImg from './image/buyHead.png'
-import sellerHouseIcon from './image/sellerHouseIcon.svg'
-import sellerpageIcon from './image/sellerpageIcon.svg'
-import sellerupIcon from './image/sellerupIcon.svg'
-import sellerlistIcon from './image/sellerlistIcon.svg'
-import sellerorderIcon from './image/sellerorderIcon.svg'
+import buyerImg from '../image/buyHead.png'
+import sellerHouseIcon from '../image/sellerHouseIcon.svg'
+import sellerpageIcon from '../image/sellerpageIcon.svg'
+import sellerupIcon from '../image/sellerupIcon.svg'
+import sellerlistIcon from '../image/sellerlistIcon.svg'
+import sellerorderIcon from '../image/sellerorderIcon.svg'
 
 
 function SellerHome() {
@@ -64,55 +63,70 @@ function SellerHome() {
           </div>
           <div></div> */}
         </div>
+        <div className="_sellerhome__pic_414 m-3">
+              <img src={buyerImg} alt="sellerHead" className="_sellerhome_headImg" />
+              <label className='sellerhome__headIcon_414'>
+                {/* 增加檔案 */}
+              <input type="file" style={{ display: "none" }}></input>
+              </label>
+        </div>
         <div className="sellerhome__sidebar">
           <div className="sellerhome__sidebar__center">
             <Link to="/">
-              <img src={logo1} />
+              <img className="sellerhome__sidebar__center_logo" src={logo1} />
             </Link>
-            <div className="_sellerhome__pic m-3">
+            <div className="_sellerhome__pic_1920 m-3">
               <img src={buyerImg} alt="sellerHead" className="_sellerhome_headImg" />
               <label className='sellerhome__headIcon'>
                 {/* 增加檔案 */}
               <input type="file" style={{ display: "none" }}></input>
               </label>
             </div>
-            <ul className="list-unstyled">
-              <li>
+            <div>
+            <ul className="list-unstyled sellerhome__icon">
+              <li className="d-flex">
+              
                 <SellerButton
-                  text="首頁"
+                  className="sellerhome__icon_botton"
                   src={sellerHouseIcon}
                   onClick={SellerFrontPage}
                 />
+               <p className="sellerhome__icon_text" >首頁</p>
               </li>
-              <li>
+              <li className="d-flex">
                 <SellerButton 
-                  text="個人頁面"
+                  className="sellerhome__icon_botton"
                   src={sellerpageIcon}
                   onClick={SellerPage}
                 />
+                <p className="sellerhome__icon_text">個人頁面</p>
               </li>
-              <li>
+              <li className="d-flex">
                 <SellerButton
-                  text="上架商品"
+                  className="sellerhome__icon_botton"
                   src={sellerupIcon}
                   onClick={SellerUpload}
                 />
+                <p className="sellerhome__icon_text">上架商品</p>
               </li>
-              <li>
+              <li className="d-flex">
                 <SellerButton 
-                  text="商品管理"
+                  className="sellerhome__icon_botton"
                   src={sellerlistIcon}
                   onClick={SellerProduct}
                 />
+              <p className="sellerhome__icon_text">商品管理</p>  
               </li>
-              <li>
+              <li className="d-flex">
                 <SellerButton
-                  text="訂單資訊"
+                  className="sellerhome__icon_botton"
                   src={sellerorderIcon}
                   onClick={SellerOrder}
                 />
+                <p className="sellerhome__icon_text">訂單資訊</p>
               </li>
             </ul>
+            </div>
           </div>
         </div>
       </div>
