3b40d34fa591b82fa8b6eb3d41eb3f4177f59e19
y343632 <117623833+y343632@users.noreply.github.com>
2023-02-06 21:48:45 +0800

    顏色篩選完成

--- src/containers/products/Products.jsx
+++ src/containers/products/Products.jsx
@@ -4,6 +4,8 @@ import { Link   } from 'react-router-dom'
 import './products.css'
 import Dropdown from 'react-bootstrap/Dropdown'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+//篩選區域
 import ProductsSize_Slider from './ProductsSize_Slider'
 import ProductsPrice_Slider from './ProductsPrice_Slider'
 
@@ -14,16 +16,9 @@ import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import ProductsPagination from './ProductsPagination'
 import { RiArrowUpSLine,RiArrowDownSLine } from 'react-icons/ri'
-<<<<<<< HEAD
 
 function Products  ()  {
   window.scrollTo(0, 800);
-=======
-
-
-function Products  ()  {
-  window.scrollTo(0, 1200);
->>>>>>> b4a8102661693778d01695d89c83378f78031ff6
   const [product, setProducts] = useState([])
   //product初始值
   const [originalProduct, setOriginalProducts] = useState([])
@@ -31,13 +26,13 @@ function Products  ()  {
   const [selectedMaterial, setSelectedMaterial] = useState('')
   const [selectedCreation_year, setSelectedCreation_year] = useState('')
   const [selectedWork_hue, setSelectedWork_hue] = useState('')
-<<<<<<< HEAD
+
   const [selectedPrice, setSelectedPrice] = useState('')
 
   // radio
   const [priceRange, setPriceRange] = useState('所有')
   const priceRangeTypes = ['所有', '2000-9999', '10000-19999', '20000-29999', '30000-49999','100000']
-  const [searchWord, setSearchWord] = useState('')
+ 
 
   
     //清除鍵
@@ -56,51 +51,13 @@ function Products  ()  {
   
     }
     const [currentPage, setCurrentPage] = useState(1)
-    const [postsPerPage, setPostPerPage] = useState(15)
+    const [postsPerPage, setPostPerPage] = useState(13)
     const lastPostIndex = currentPage * postsPerPage
     const firstPostIndex = lastPostIndex - postsPerPage
     const currentPosts = product.slice(firstPostIndex, lastPostIndex)
     const pages = Math.ceil(product.length / postsPerPage)
-    console.log(currentPage,product.length,pages)
+    //console.log(currentPage,product.length,pages)
   
-=======
-  
-    //清除鍵
-    const handleClear = () => {
-      //清空初始值
-      setProducts(originalProduct)
-      console.log(originalProduct)
-      //清空媒材
-      setSelectedMaterial('')
-      //清空年份
-      setSelectedCreation_year('')
-      //清空顏色
-      setSelectedWork_hue([]);
-  
-    }
-    const [currentPage, setCurrentPage] = useState(1)
-    const [postsPerPage, setPostPerPage] = useState(7)
-    const lastPostIndex = currentPage * postsPerPage
-    const firstPostIndex = lastPostIndex - postsPerPage
-    const currentPosts = product.slice(firstPostIndex, lastPostIndex)
-    const pages = Math.ceil(product.length / postsPerPage)
-    console.log(currentPage,product.length,pages)
-
-
-  // useEffect(() => {
-  //   //console.log('第二個參數是空陣列');
-  //   // 在 component 初始化的時候跑一次
-  //   // 通常會把去跟後端要資料的動作放在這裡
-  //   async function getProducts() {
-  //     let response = await axios.get(`http://localhost:3001/product?page=${page}`);
-  //     setProducts(response.data.data);
-  //     console.log(response.data.data);
-  //     setTotalPage(response.data.pagination.totalPage);
-  //   }
-  //   getProducts();
-  // }, [page]);
-  
->>>>>>> b4a8102661693778d01695d89c83378f78031ff6
   useEffect(() => {
     console.log('空陣列的 useEffect');
   }, []);
@@ -118,11 +75,7 @@ function Products  ()  {
     getProducts();
   }, []);
 
-<<<<<<< HEAD
   
-=======
- 
->>>>>>> b4a8102661693778d01695d89c83378f78031ff6
   //選擇媒材
   const handleClick = (value, type) => {
     //先設定一個filter(符合條件的新陣列)值
@@ -139,7 +92,7 @@ function Products  ()  {
     }
       // 處理依照年份排序
     if (type === 'creation_year') {
-        const sortOrder = value;
+        const sortOrder = value
         filtered = [...filtered].sort((a, b) => {
           if (sortOrder === '由舊至新') {
             return a.creation_year - b.creation_year;
@@ -152,107 +105,35 @@ function Products  ()  {
 
     if (type === 'work_hue') {
       // 處理顏色選項
-      const work_hue = value
-      filtered = product.filter(product => product.work_hue);
-      setProducts(filtered);
+      const work_hue = value  
       //顏色的陣列(重複不出現)
-      if (selectedWork_hue) {
-        setSelectedWork_hue(selectedWork_hue.filter(d => d !== work_hue));
+      let newSelectedWork_hue = [...selectedWork_hue]  
+
+      if (newSelectedWork_hue.includes(work_hue)) {
+        newSelectedWork_hue = selectedWork_hue.filter ((d) => d !== work_hue)
+        setSelectedWork_hue(selectedWork_hue.filter((d) => d !== work_hue))
       } else {
-        setSelectedWork_hue([...selectedWork_hue, work_hue]);
+        newSelectedWork_hue = [...selectedWork_hue, work_hue]
+        setSelectedWork_hue([...selectedWork_hue, work_hue])
       }
+      //filtered=filtered.filter((product)=>product.work_hue===work_hue)
+
+      const newFiltered = filtered.filter((prodcut, i) => {
+        let found = false
+        for (let i = 0; i < newSelectedWork_hue.length; i++) {
+          if (prodcut.work_hue.includes(newSelectedWork_hue[i])) {
+            found = true
+            break
+          }
+        }
+        return found
+      })
+      setProducts(newFiltered)
+
+      console.log(filtered.length, newSelectedWork_hue, newFiltered.length)
+      console.log('顏色選項：', newSelectedWork_hue)
     }
   }
-<<<<<<< HEAD
-=======
-  
-  // const getPages = () => {
-  //   let pages = [];
-  
-  //   // 顯示「上一頁」
-  //   if (page > 1) {
-  //     pages.push(
-  //       <li
-  //         style={{
-  //           display: 'inline-block',
-  //           margin: '2px',
-  //           backgroundColor: 'black',
-  //           borderColor: '#dbdbdb',
-  //           color: '#363636',
-  //           borderWidth: '1px',
-  //           width: '28px',
-  //           height: '28px',
-  //           borderRadius: '3px',
-  //           textAlign: 'center',
-  //         }}
-  //         key={-1}
-  //         onClick={(e) => {
-  //           setPage(page - 1);
-  //           navigate(`/products?page=${page - 1}`);
-  //         }}
-  //       >
-  //         {'<'}
-  //       </li>
-  //     );
-  //   }
-  
-  //   // 顯示頁碼
-  //   for (let i = 1; i <= 5; i++) {
-  //     pages.push(
-  //       <li
-  //         style={{
-  //           display: 'inline-block',
-  //           margin: '2px',
-  //           backgroundColor: 'black',
-  //           borderColor: page === i ? '#00d1b2' : '#dbdbdb',
-  //           color: page === i ? '#fff' : '#363636',
-  //           borderWidth: '1px',
-  //           width: '28px',
-  //           height: '28px',
-  //           borderRadius: '3px',
-  //           textAlign: 'center',
-  //         }}
-  //         key={i}
-  //         onClick={(e) => {
-  //           setPage(i);
-  //           navigate(`/products?page=${i}`);
-  //         }}
-  //       >
-  //         {i}
-  //       </li>
-  //     );
-  //   }
-  
-  //   // 顯示「下一頁」
-  //   if (page < totalPage) {
-  //     pages.push(
-  //       <li
-  //         style={{
-  //           display: 'inline-block',
-  //           margin: '2px',
-  //           backgroundColor: 'black',
-  //           borderColor: '#dbdbdb',
-  //           color: '#363636',
-  //           borderWidth: '1px',
-  //           width: '28px',
-  //           height: '28px',
-  //           borderRadius: '3px',
-  //           textAlign: 'center',
-  //         }}
-  //         key={-2}
-  //         onClick={(e) => {
-  //           setPage(page + 1);
-  //           navigate(`/products?page=${page + 1}`);
-  //         }}
-  //       >
-  //         {'>'}
-  //       </li>
-  //     );
-  //   }
-  
-  //   return pages;
-  // };
->>>>>>> b4a8102661693778d01695d89c83378f78031ff6
 
   // 處理價格區間選項
   const handlePriceRange = (value, type) => {
@@ -308,8 +189,6 @@ function Products  ()  {
     // return Productsfilter
   }
 
-  
-
   return (
     <>
       <header id="Products__header">
@@ -324,7 +203,6 @@ function Products  ()  {
               <th>
                 <h3 className="Products＿size-h3">藝術品分類</h3>
               </th>
-              
               <tr>
               <Link className='ml-4' variant="dark" onClick={handleClear}>清除選取</Link>
                 <Dropdown>
@@ -362,7 +240,7 @@ function Products  ()  {
                 </Dropdown>
               </tr>
               <tr>
-                {/* <Dropdown>
+                <Dropdown>
                   <Dropdown.Toggle
                     variant="--color-bg"
                     style={{ border: 'none' }}
@@ -382,59 +260,107 @@ function Products  ()  {
                       表現主義(20)
                     </Dropdown.Item>
                   </Dropdown.Menu>
-                </Dropdown> */}
+                </Dropdown>
               </tr>
               <h6 className="Products＿size-h5">依色系</h6>
+              
               <tr className="Products＿slider-color">
                 <table>
                   <tr>
+                  {['Red'].map(
+                    (work_hue) => (
                     <td>
                       <div className="Products＿slider-color-item Products＿slider-color-item-red"
-                      onClick={() => handleClick('red', 'work_hue') }
-                      >{selectedWork_hue}</div>
+                      value={work_hue}
+                      onClick={() => handleClick(work_hue, 'work_hue') }
+                      ></div>
                     </td>
+                       )
+                       )}
+                    {['Orange'].map(
+                    (work_hue) => (
                     <td>
                       <div className="Products＿slider-color-item Products＿slider-color-item-ori"
-                      onClick={() => handleClick('orange', 'work_hue') }
-                      >{selectedWork_hue}</div>
-                    </td>
+                      value={'work_hue'}
+                      onClick={() => handleClick(work_hue, 'work_hue') }
+                      ></div>
+                    </td> 
+                    )
+                    )}
+                    {['Yellow'].map(
+                    (work_hue) => (
                     <td>
                       <div className="Products＿slider-color-item Products＿slider-color-item-yel"
-                      onClick={() => handleClick('yellow', 'work_hue') }
-                      >{selectedWork_hue}</div>
+                      value={'Yellow'}
+                      onClick={() => handleClick(work_hue, 'work_hue') }
+                      ></div>
                     </td>
+                     )
+                    )}
+                    {['Green'].map(
+                    (work_hue) => (
                     <td>
                       <div className="Products＿slider-color-item Products＿slider-color-item-gre"
-                      onClick={() => handleClick('green', 'work_hue') }
-                      >{selectedWork_hue}</div>
+                      value={'Green'}
+                      onClick={() => handleClick(work_hue, 'work_hue') }
+                      ></div>
                     </td>
+                      )
+                     )}
                   </tr>
                   <tr>
+                  {['Blue'].map(
+                    (work_hue) => (
                     <td>
                       <div className="Products＿slider-color-item Products＿slider-color-item-blu"
-                      onClick={() => handleClick('blue', 'work_hue') }
-                      >{selectedWork_hue}</div>
+                      value={'Blue'}
+                      onClick={() => handleClick(work_hue, 'work_hue') }
+                      ></div>
                     </td>
+                     )
+                     )}
+                    {['Purple'].map(
+                    (work_hue) => (
                     <td>
-                      <div className="Products＿slider-color-item Products＿slider-color-item-pur">{selectedWork_hue}</div>
+                      <div className="Products＿slider-color-item Products＿slider-color-item-pur" value={'Purple'} onClick={() => handleClick(work_hue, 'work_hue') }></div>
                     </td>
+                     )
+                     )}
+                     {['Brown'].map(
+                    (work_hue) => (
                     <td>
-                      <div className="Products＿slider-color-item Products＿slider-color-item-bro">{selectedWork_hue}</div>
+                      <div className="Products＿slider-color-item Products＿slider-color-item-bro" value={'Brown'} onClick={() => handleClick(work_hue, 'work_hue') }></div>
                     </td>
+                    )
+                    )}
+                    {['White'].map(
+                    (work_hue) => (
                     <td>
-                      <div className="Products＿slider-color-item Products＿slider-color-item-whi">{selectedWork_hue}</div>
+                      <div className="Products＿slider-color-item Products＿slider-color-item-whi" value={'White'} onClick={() => handleClick(work_hue, 'work_hue') }></div>
                     </td>
+                    )
+                    )}
                   </tr>
                   <tr>
+                  {['Gray'].map(
+                    (work_hue) => (
                     <td>
-                      <div className="Products＿slider-color-item Products＿slider-color-item-gra">{selectedWork_hue}</div>
+                      <div className="Products＿slider-color-item Products＿slider-color-item-gra" value={'Gray'} onClick={() => handleClick(work_hue, 'work_hue') }></div>
                     </td>
+                    )
+                    )}
+                  {['Black'].map(
+                  (work_hue) => (
                     <td>
-                      <div className="Products＿slider-color-item Products＿slider-color-item-bla">{selectedWork_hue}</div>
-                    </td>
+                      <div className="Products＿slider-color-item Products＿slider-color-item-bla" value={'Black'} onClick={() => handleClick(work_hue, 'work_hue') } ></div>
+                    </td> 
+                    )
+                    )}
                   </tr>
                 </table>
+              
               </tr>
+              
              <h6 className="Products＿size-h5">形狀</h6>
               <tr className="Products＿slider-shape">
                 <BiRectangle style={{ width: '40px', height: '40px' }} />
@@ -453,7 +379,6 @@ function Products  ()  {
                 <ProductsSize_Slider></ProductsSize_Slider>
               </tr>
               <h5 className="Products＿size-h5">金額篩選</h5>
-<<<<<<< HEAD
               {/* <tr className="Products＿slider-price">
                 <ProductsPrice_Slider
                 priceRangeTypes={priceRangeTypes}
@@ -503,11 +428,6 @@ function Products  ()  {
                   >≤100000</Dropdown.Item>
                   </Dropdown.Menu>
                 </Dropdown>
-=======
-              <tr className="Products＿slider-price">
-                <ProductsPrice_Slider></ProductsPrice_Slider>
-              </tr>
->>>>>>> b4a8102661693778d01695d89c83378f78031ff6
             </table>
           </aside>
           <nav id="Products＿nav">
@@ -518,7 +438,7 @@ function Products  ()  {
               <div className="Products＿link-bar ">
                 <p className="Products＿link d-flex">
                   <Link to="/">首頁▶</Link>
-                  <Link onClick={handleClear}>所有藝術品▶ {selectedMaterial}</Link>
+                  <Link onClick={handleClear}>所有藝術品▶{selectedMaterial} ▶ {selectedWork_hue}</Link>
                 </p>
            <Dropdown>
                   <Dropdown.Toggle
