c36a9705e8e09792b31e9f56f72709a328e7f679
y343632 <117623833+y343632@users.noreply.github.com>
2023-02-08 14:46:36 +0800

    Add files via upload

--- src/containers/users/Sell/SellerHome.jsx
+++ src/containers/users/Sell/SellerHome.jsx
@@ -26,43 +26,9 @@ function SellerHome() {
     <>
       <div className="d-flex">
         <div className="sellerhome__main" id="sellerhome__main">
-          {/* <div className="sellerhome__main__section__news">
-            <h2>最新消息</h2>
-            <p>這是第一則重要消息！！看的到代表一切正常不用擔心，看不到代表要去客服反應！</p>
-          </div>
-          <div className="sellerhome__main__section__todolist">
-            <h2>待辦事項</h2>
-            <div>
-              <ul className="list-unstyled">
-                <li>
-                  <Link><h1>0</h1></Link>
-                  <h3>未出貨</h3>
-                </li>
-                <li>
-                  <Link><h1>0</h1></Link>
-                  <h3>待處理</h3>
-                </li>
-                <li>
-                  <Link><h1>0</h1></Link>
-                  <h3>已處理</h3>
-                </li>
-                <li>
-                  <Link><h1>0</h1></Link>
-                  <h3>待取消</h3>
-                </li>
-                <li>
-                  <Link><h1>0</h1></Link>
-                  <h3>待退貨</h3>
-                </li>
-                <li>
-                  <Link><h1>0</h1></Link>
-                  <h3>已售完</h3>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div></div> */}
         </div>
+
+        {/* 手機板 */}
         <div className="_sellerhome__pic_414 m-3">
               <img src={buyerImg} alt="sellerHead" className="_sellerhome_headImg" />
               <label className='sellerhome__headIcon_414'>
@@ -70,6 +36,8 @@ function SellerHome() {
               <input type="file" style={{ display: "none" }}></input>
               </label>
         </div>
+
+
         <div className="sellerhome__sidebar">
           <div className="sellerhome__sidebar__center">
             <Link to="/">
@@ -85,7 +53,6 @@ function SellerHome() {
             <div>
             <ul className="list-unstyled sellerhome__icon">
               <li className="d-flex">
-              
                 <SellerButton
                   className="sellerhome__icon_botton"
                   src={sellerHouseIcon}
