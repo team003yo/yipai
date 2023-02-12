388d0051f44ef6e5031a5e7ebc05ec9b7a7bf0d8
y343632 <117623833+y343632@users.noreply.github.com>
2023-02-12 15:18:10 +0800

    News RWD、篩選、分頁ＯＫ

--- src/containers/news/News.jsx
+++ src/containers/news/News.jsx
@@ -1,128 +1,25 @@
 // import React from 'react'
+import React, { useState } from 'react'
 import './news.css'
 import { Link } from 'react-router-dom'
+import news__banner from '../../assets/news__banner.png'
 import Button from 'react-bootstrap/Button'
 import { TbArrowsSort } from 'react-icons/tb'
 import { BiTimeFive } from 'react-icons/bi'
-import { MdArrowForwardIos,MdArrowBackIosNew } from 'react-icons/md'
-import { RiArrowUpSLine,RiArrowDownSLine } from 'react-icons/ri'
-import axios from 'axios';
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios'
+import { useEffect } from 'react'
 
-
-function News  ()  {
-  window.scrollTo(0, 0);
-  // 為了處理網址
-  let navigate = useNavigate();
-  const { currentPage } = useParams();
-  const [page, setPage] = useState(parseInt(currentPage, 10) || 1); // 目前在哪一頁
-  const [totalPage, setTotalPage] = useState(); // 總共有幾頁
-
-  const [news, setNews] = useState([]);
-
-  useEffect(()=>{
-    console.log('空陣列的 useEffect');
-  },[]);
-
+const News = () => {
+  const [news, setNews] = useState([])
   useEffect(() => {
-    console.log('第二個參數是空陣列');
-    // 在 component 初始化的時候跑一次
-    // 通常會把去跟後端要資料的動作放在這裡
     async function getNews() {
-      let response = await axios.get(`http://localhost:3001/news?page=${page}`);
-      setNews(response.data.data);
-      console.log(response.data.data);
-      setTotalPage(response.data.pagination.totalPage);
+      let response = await axios.get('http://localhost:3001/news')
+      setNews(response.data)
     }
-    getNews();
-  }, [page]);
+    getNews()
+  }, [])
 
-  
-  const getPages = () => {
-    let pages = [];
-  
-    // 顯示「上一頁」
-    if (page > 1) {
-      pages.push(
-        <RiArrowUpSLine
-          style={{
-            display: 'inline-block',
-            margin: '2px',
-            borderColor: '#dbdbdb',
-            borderWidth: '1px',
-            width: '50px',
-            height: '50px',
-            borderRadius: '3px',
-            textAlign: 'center',
-            
-          }}
-          key={-1}
-          onClick={(e) => {
-            setPage(page - 1);
-            navigate(`/news?page=${page - 1}`);
-          }}
-        >
-          {'<'}
-        </RiArrowUpSLine>
-      );
-    }
-  
-    // 顯示頁碼
-    // for (let i = 1; i <= 5; i++) {
-    //   pages.push(
-    //     <li
-    //       style={{
-    //         display: 'inline-block',
-    //         margin: '2px',
-    //         backgroundColor: 'black',
-    //         borderColor: page === i ? '#00d1b2' : '#dbdbdb',
-    //         color: page === i ? '#fff' : '#363636',
-    //         borderWidth: '1px',
-    //         width: '28px',
-    //         height: '28px',
-    //         borderRadius: '3px',
-    //         textAlign: 'center',
-    //       }}
-    //       key={i}
-    //       onClick={(e) => {
-    //         setPage(i);
-    //         navigate(`/news?page=${i}`);
-    //       }}
-    //     >
-    //       {i}
-    //     </li>
-    //   );
-    // }
-  
-    // 顯示「下一頁」
-    if (page < totalPage) {
-      pages.push(
-        <RiArrowDownSLine
-          style={{
-            display: 'flex',
-            margin: '2px',
-            borderColor: '#dbdbdb',
-            borderWidth: '1px',
-            width: '50px',
-            height: '50px',
-            borderRadius: '3px',
-            textAlign: 'center',
-            
-          }}
-          key={-2}
-          onClick={(e) => {
-            setPage(page + 1);
-            navigate(`/news?page=${page + 1}`);
-          }}
-        >
-          
-        </RiArrowDownSLine>
-      );
-    }
-  
-    return pages;
-  };
+  // const [toggleMenu, setToggleMenu] = useState(false)
 
   return (
     <div>
@@ -144,7 +41,7 @@ function News  ()  {
               </Link>
             </div>
             <div className="col-7">
-              <Button className="News__button mt-2" variant="dark">
+              <Button className="News__button" variant="dark">
                 由新至舊
                 <TbArrowsSort />
               </Button>
@@ -170,9 +67,7 @@ function News  ()  {
             })}
           </div>
         </main>
-        <ul>{getPages()}</ul>
       </section>
-     
     </div>
   )
 }
--- src/containers/news/NewsId.jsx
+++ src/containers/news/NewsId.jsx
@@ -1,32 +1,21 @@
 // import React from 'react'
-import React, { useState } from "react";
-import "./newsId.css";
-import { FaHeart } from "react-icons/fa";
-import { FiShare } from "react-icons/fi";
-import { IoIosTimer } from "react-icons/io";
-import { FaMapMarker } from "react-icons/fa";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
-
-//地圖
-import {
-  MapContainer as LeafletMap,
-  TileLayer,
-  Marker,
-  Popup,
-} from "react-leaflet";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
-import { margin } from "@mui/system";
+import React, { useState } from 'react'
+import './newsId.css'
+import { FaHeart } from 'react-icons/fa'
+import { FiShare } from 'react-icons/fi'
+import { IoIosTimer } from 'react-icons/io'
+import { FaMapMarker } from 'react-icons/fa'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 const NewsId = () => {
-  const { newsId } = useParams();
-  const [data, setdata] = useState([]);
+  const { newsId } = useParams()
+  const [data, setdata] = useState([])
 
   useEffect(() => {
     // console.log('第二個參數是空陣列')
@@ -34,120 +23,83 @@ const NewsId = () => {
     // 通常會把去跟後端要資料的動作放在這裡
 
     async function getdata() {
-      let response = await axios.get(`http://localhost:3001/news/${newsId}?`);
-      setdata(response.data);
+      let response = await axios.get(`http://localhost:3001/news/${newsId}?`)
+      setdata(response.data)
       // console.log(response.data)
     }
-    getdata();
-  }, []);
-
-  const customMarker = new L.Icon({
-    iconUrl: "https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon.png",
-    // iconSize: [25, 41],
-    // iconAnchor: [10, 41],
-    // popupAnchor: [20, -40],
-  });
+    getdata()
+  }, [])
 
   return (
     <div>
-      <div className="news__container">
+      <div className="container">
         {data.map((newsDetail) => {
           return (
-            <div className="news__wrapper">
+            <div className="wrapper">
               <img
-                className="news__yipai__img"
+                className="yipai__img"
                 src={newsDetail.img_file}
                 alt="banner"
               />
               <div>
-                <div className="d-flex flex-row justify-content-center my-4 news__a">
+                <div className="d-flex flex-row justify-content-center my-4 a">
                   <div className="yipai_newsId">
-                    <div className="news__timericon">
-                      <box className="news__collect">
+                    <div className="icon">
+                      <box className="collect">
                         <Link to="#">
                           <p>
-                            <FaHeart className="news__FaHeart_icon" />
+                            <FaHeart className="FaHeart_icon" />
                             收藏
                           </p>
                         </Link>
                       </box>
-                      <box className="news__share">
+                      <box className="share">
                         <Link to="#">
                           <p>
-                            {" "}
-                            <FiShare className="news__share_icon" />
+                            {' '}
+                            <FiShare className="share_icon" />
                             分享
                           </p>
                         </Link>
                       </box>
                     </div>
                     <div key={newsDetail.id}>
-                      <h1 className="news__fw-bold">{newsDetail.name}</h1>
+                      <h1 className="fw-bold">{newsDetail.name}</h1>
                       <div className="d-flex flex-row justify-content-center my-2 ">
-                        <IoIosTimer className="news__timer_icon" />
+                        <IoIosTimer className="timer_icon" />
                         {newsDetail.news_time}
                       </div>
                     </div>
-                    
-                      <Row className="news__row">
+                    <Container>
+                      <Row>
+                        <Col>
                           <p className="newsDetail__text" align="left">
                             {newsDetail.news_introduction}
                           </p>
-                      
+                        </Col>
                         <div className="newsDetail_address d-flex flex-row justify-content-center ">
-                          <h5 className="newsDetail_address-icon">
-                            {" "}
-                            <FaMapMarker />
-                            地點
-                          </h5>
+                          <h5 className='newsDetail_address-icon'> <FaMapMarker />地點</h5>
                           <h5> {newsDetail.news_address}</h5>
+
                         </div>
 
-                        <div className="news__google">
-                          <LeafletMap
-                            center={[
-                              newsDetail.news_lat,
-                              newsDetail.news_lng,
-                            ]}
-                            zoom={22}
-                            style={{ height: "50vh", width: "100%", margin: "auto" }}
-                          >
-                          
-                                  
-                            <TileLayer
-                              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                              url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
-                            />
-                            {/* {state.map(({ lat, lng }, index) => ( */}
-                            <Marker
-                              position={[
-                                newsDetail.news_lat,
-                                newsDetail.news_lng,
-                              ]}
-                              icon={customMarker}
-                              // key={index}
-                            >
-                              {/* <Popup>
-                            {index + 1} is for popup with lat: {lat} and lon {lng}
-                              </Popup> */}
-                            </Marker>
-                            {/* ))} */}
-                             </LeafletMap>
-                            {/* <img
+                        <div className="mapC">
+                          <img
                             className="map__img"
-                            src={require("./Map.jpg")}
-                          /> */}
+                            src={require('./Map.jpg')}
+                          />
                         </div>
                       </Row>
+                    </Container>
                   </div>
                 </div>
               </div>
             </div>
-          );
+          )
         })}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default NewsId;
+export default NewsId
